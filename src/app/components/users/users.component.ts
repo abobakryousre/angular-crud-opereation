89659e240d41567a0fcbfdf13b8795aad5367b8d
Abobakr <abobakryousre@gmail.com>
2021-05-02 17:53:23 +0200

    update style in  users component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -15,14 +14,10 @@ export class UsersComponent implements OnInit {
   }
 
   users:{id:number, name:string, email:string, city:string}[] = [{id:1, name:"abobark", email:"abobakr@gmail.com" , city:"cairo"}]
-  userForm:FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)] ),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    city: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-  })
 
-  addUser(){
-    console.log(this.userForm.value);
+  editUser(id:number){
   }
+  deleteUser(id:number){
 
+  }
 }
